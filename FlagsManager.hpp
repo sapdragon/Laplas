fd21a85b9a7a21e54368c4cfd3e6d2b33ba3a2c9
SapDragon <74473217+SapDragon@users.noreply.github.com>
2022-07-15 19:37:41 +0200

    code formatting

--- FlagsManager.hpp
+++ FlagsManager.hpp
@@ -1,6 +1,8 @@
 #pragma once
+
 #include <variant>
 #include "../Utils/Utils.hpp"
+
 enum ProcessorStatuses_t
 {
 	Carry = 0,
@@ -12,22 +14,24 @@ enum ProcessorStatuses_t
 	NegativeResult,
 };
 
-class C_FlagManager 
+class C_FlagManager
 {
-	public:
-	C_FlagManager ( uint8_t& iFlags)
+public:
+	C_FlagManager(uint8_t &iFlags)
 	{
 		m_iFlags = iFlags;
 	}
-	
+
 	void SetFlag(ProcessorStatuses_t iFlag, bool bStatus)
 	{
-	    Utils::SetBit(this->m_iFlags, iFlag, bStatus);
+		Utils::SetBit(this->m_iFlags, iFlag, bStatus);
 	}
+
 	bool GetFlagStatus(ProcessorStatuses_t iFlag)
 	{
 		return Utils::GetBit(this->m_iFlags, iFlag);
 	}
-	private:
-	uint8_t& m_iFlags;
+	
+private:
+	uint8_t &m_iFlags;
 };
--- Processor.hpp
+++ Processor.hpp
@@ -1,8 +1,10 @@
 #include <cstdint>
 #include  <vector>
 #include "FlagsManager.hpp"
+
 #define STATIC_MEMORY_PAGES 8
 #define MEMORY_PAGE_SIZE 256
+
 enum AddressingMode_t
 {
 	Implied = 2,
@@ -45,63 +47,63 @@ class C_ProcessorContext
 
 class C_Processor : private C_ProcessorContext
 {
-	public:
+public:
 	/* Registers read/write */
-	   void LoadAccumulatorWithMemory( AddressingMode_t iMode, uint16_t nAddress);
-	   void LoadIndexXWithMemory( AddressingMode_t iMode, uint16_t nAddress);
-	   void LoadIndexYWithMemory( AddressingMode_t iMode, uint16_t nAddress);
+	void LoadAccumulatorWithMemory( AddressingMode_t iMode, uint16_t nAddress);
+	void LoadIndexXWithMemory( AddressingMode_t iMode, uint16_t nAddress);
+	void LoadIndexYWithMemory( AddressingMode_t iMode, uint16_t nAddress);
 	   
-	   void StoreIndexYInMemory( AddressingMode_t iMode, uint16_t nAddress);
-	   void StoreIndexXInMemory( AddressingMode_t iMode, uint16_t nAddress);
-	   void StoreAccumulatorInMemory( AddressingMode_t iMode, uint16_t nAddress);
+	void StoreIndexYInMemory( AddressingMode_t iMode, uint16_t nAddress);
+	void StoreIndexXInMemory( AddressingMode_t iMode, uint16_t nAddress);
+	void StoreAccumulatorInMemory( AddressingMode_t iMode, uint16_t nAddress);
 	   
-	   void TransferAccumulatorToIndexX();
-	   void TransferAccumulatorToIndexY();
-	   void TransferIndexXToAccumulator();
-	   void TransferIndexYToAccumulator();
+	void TransferAccumulatorToIndexX();
+	void TransferAccumulatorToIndexY();
+	void TransferIndexXToAccumulator();
+	void TransferIndexYToAccumulator();
 	   
-	  void TransferStackPointerToIndexX();
-	  void TransferIndexXToStackPointer();
+	void TransferStackPointerToIndexX();
+	void TransferIndexXToStackPointer();
 	  
 	  /* Arithmetic bits */
-	  void AndWithAccumulator( AddressingMode_t iMode, uint16_t nAddress);
-	  void XorWithAccumulator( AddressingMode_t iMode, uint16_t nAddress);
-	   void OrWithAccumulator( AddressingMode_t iMode, uint16_t nAddress);	  
-	   /* Increment / Decrement index register*/
-	  void IncrementIndexX();
-	  void DecrementIndexX();
+	void AndWithAccumulator( AddressingMode_t iMode, uint16_t nAddress);
+	void XorWithAccumulator( AddressingMode_t iMode, uint16_t nAddress);
+	void OrWithAccumulator( AddressingMode_t iMode, uint16_t nAddress);	  
 
-	  void IncrementIndexY();
-	  void DecrementIndexY();
+	/* Increment / Decrement index register*/
+	void IncrementIndexX();
+	void DecrementIndexX();
+
+	void IncrementIndexY();
+	void DecrementIndexY();
 	  
-	  /* Increment / Decrement value in memory*/
-	  void IncrementMemoryByOne(AddressingMode_t iMode, uint16_t nAddress);
-	  void DecrementMemoryByOne(AddressingMode_t iMode, uint16_t nAddress);
+	/* Increment / Decrement value in memory*/
+	void IncrementMemoryByOne(AddressingMode_t iMode, uint16_t nAddress);
+	void DecrementMemoryByOne(AddressingMode_t iMode, uint16_t nAddress);
 	  
-	  /* Compare instructuons */
-	  void CompareMemoryWithAccumulator(AddressingMode_t iMode, uint16_t nAddress); 
-	  void CompareMemoryWithIndexX(AddressingMode_t iMode, uint16_t nAddress); 
-	  void CompareMemoryWithIndexY(AddressingMode_t iMode, uint16_t nAddress);
+	/* Compare instructuons */
+	void CompareMemoryWithAccumulator(AddressingMode_t iMode, uint16_t nAddress); 
+	void CompareMemoryWithIndexX(AddressingMode_t iMode, uint16_t nAddress); 
+	void CompareMemoryWithIndexY(AddressingMode_t iMode, uint16_t nAddress);
 	  
-	  /* Rotate bits*/
-	 void ShiftOneBitRight(AddressingMode_t iMode, uint16_t nAddress);	 
-	 	 void ShiftOneBitLeft(AddressingMode_t iMode, uint16_t nAddress);
+	/* Rotate bits*/
+	void ShiftOneBitRight(AddressingMode_t iMode, uint16_t nAddress);	 
+	void ShiftOneBitLeft(AddressingMode_t iMode, uint16_t nAddress);
 	 	 
-	 	 	 void RotateOneBitRight(AddressingMode_t iMode, uint16_t nAddress);	 
-	 	 void RotateOneBitLeft(AddressingMode_t iMode, uint16_t nAddress);
-	private:
-	
+	void RotateOneBitRight(AddressingMode_t iMode, uint16_t nAddress);	 
+	void RotateOneBitLeft(AddressingMode_t iMode, uint16_t nAddress);
+
+private:
 	/* Why need SEC, CLC, CLV etc..*/
 	C_FlagManager m_cFlagManager;
 	
 	/* Get the specified number of bytes from memory at the address */
     std::vector<uint8_t> GetDataFromMemory(uint16_t nAddress, int iSize = 1);
 	
-		/* Set the specified number of bytes to memory at the address */
+	/* Set the specified number of bytes to memory at the address */
     void SetDataToMemory(uint16_t nAddress, std::vector<uint8_t> vecData);
 	
 	/* Get data depending on the addressing mode  */
 	uint16_t GetDataFromMnemonic(AddressingMode_t iMode, uint16_t nMnemonic);
-	 void SetDataFromMnemonic(AddressingMode_t iMode, uint16_t nMnemonic, std::vector<uint8_t> vecData);
-	 
+	void SetDataFromMnemonic(AddressingMode_t iMode, uint16_t nMnemonic, std::vector<uint8_t> vecData);
 };
